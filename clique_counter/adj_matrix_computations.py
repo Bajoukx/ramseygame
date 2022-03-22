17a679b15140b83869a027cdd21f2b326a6212ac
EdMDias <eduardo.m.dias@gmail.com>
2022-03-22 14:14:06 +0000

    Possibility to construct the k_tensor of a given graph only up to a fixed k.

--- clique_counter/adj_matrix_computations.py
+++ clique_counter/adj_matrix_computations.py
@@ -5,12 +5,13 @@ import numpy as np
 import torch
 
 from absl import app
+from absl import logging
 from absl import flags
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer("n_nodes", 10, "Number of nodes that the graph should have.")
-flags.DEFINE_integer("num_edges", None,
-                     "Number of nodes.")
+flags.DEFINE_integer("n_edges", None, "Number of edges of the graph.")
+flags.DEFINE_integer("max_clique_size", None, "Size of cliques to be computed to.")
 
 
 def complete_graph(n_nodes):
@@ -28,7 +29,7 @@ def random_graph(n_nodes, n_edges=None):
     """A random graph generator having a prescribed number of edges"""
 
     if n_edges is None:
-        n_edges = np.random.randint(n_nodes, np.ceil(n_nodes*(n_nodes+1)/2))
+        n_edges = np.random.randint(n_nodes, np.ceil(n_nodes * (n_nodes + 1) / 2))
 
     # generate random directed graph
     graph = dgl.rand_graph(n_nodes, n_edges)
@@ -42,19 +43,42 @@ def random_graph(n_nodes, n_edges=None):
     return graph
 
 
-def complementary_graph(graph):
+def complement_graph(graph):
+    """A function proving the complement of a given graph"""
     adj = graph.adj().to_dense()
     complete_graph_adj = torch.ones_like(adj)
     complement_graph_adj = complete_graph_adj - adj
-    src, dst = complement_graph_adj.nonzero().transpose(0,1).reshape(2, -1)
-    complement_graph = dgl.graph((src, dst))
-    complement_graph = dgl.remove_self_loop(complement_graph)
-    return complement_graph
+    src, dst = complement_graph_adj.nonzero().transpose(0, 1).reshape(2, -1)
+    complement = dgl.graph((src, dst))
+    complement = dgl.remove_self_loop(complement)
+    return complement
 
 
-def k_cliques(graph):
-    """function computing k_cliques iteratively"""
+def next_clique_multiplication(cliques, n_nodes=None):
+    """Function constructing the k+1 cliques tensor from the k cliques one"""
+
+    if n_nodes is None:
+        n_nodes = cliques.shape[0]
+
+    k = len(cliques.shape)
+    reshape_entries = (k + 1) * [n_nodes]
+
+    next_size_cliques = cliques.clone()
+    cliques = cliques.clone()  # not sure if it is needed
+    for i in range(k + 1):
+        reshape_entries[i] = 1
+        cliques = cliques.reshape(reshape_entries)
+        next_size_cliques = next_size_cliques * cliques
+        reshape_entries[i] = n_nodes
+
+    return next_size_cliques
+
+
+def k_cliques(graph, max_clique_size=None):
+    """Function computing k_cliques iteratively"""
     n_nodes = graph.num_nodes()
+    if max_clique_size is None:
+        max_clique_size = np.inf
 
     # define the 2_cliques as the entries in the adjacency matrix
     cliques = {2: graph.adj().to_dense()}
@@ -62,48 +86,52 @@ def k_cliques(graph):
     # define a stop condition to activate when there is no k_clique in a graph
     there_are_cliques = (cliques[2].any() != 0)
     k = 2
+
     while there_are_cliques:
         k += 1
-        reshape_entries = k * [n_nodes]
-        new_clique = cliques[k - 1].clone()
-        old_clique = cliques[k - 1].clone()
-        for i in range(k):
-            reshape_entries[i] = 1
-            old_clique = old_clique.reshape(reshape_entries)
-            new_clique = new_clique * old_clique
-            reshape_entries[i] = n_nodes
-        cliques[k] = new_clique
+        cliques[k] = next_clique_multiplication(cliques[k - 1], n_nodes=n_nodes)
         there_are_cliques = (cliques[k].any() != 0)
+        if not there_are_cliques:
+            logging.info('The Ramsey number of the graph is %d', k - 1)
+        there_are_cliques = there_are_cliques and (k <= max_clique_size)
 
-    # define the Ramsey number of the graph
-    ramsey_number = k-1
-    return cliques, ramsey_number
+    return cliques
 
 
-def cliques_counter(graph):
+def cliques_counter(graph, max_clique_size=None):
     """Compute the number of k cliques each node is part of"""
     n_nodes = graph.num_nodes()
-    cliques, ramsey_number = k_cliques(graph)
+    cliques = k_cliques(graph, max_clique_size=max_clique_size)
+    nr_cliques_computed = len(cliques.keys())
 
-    all_cliques = {i: n_nodes*[0.] for i in range(2, n_nodes + 1)}
-    for clique in range(2, ramsey_number + 1):
-        for i in range(n_nodes):
-            all_cliques[clique][i] = torch.sum(cliques[clique][i]).item()/(np.math.factorial(int(clique)-1))
+    if max_clique_size is None:
+        max_clique_size = n_nodes
 
-    return all_cliques
+    all_cliques = [n_nodes * [0.] for _ in range(2, max_clique_size + 1)]
+    for clique in range(2, nr_cliques_computed + 1):
+        for node in range(n_nodes):
+            all_cliques[clique - 2][node] = torch.sum(cliques[clique][node]).item() / (
+                np.math.factorial(int(clique) - 1))
+
+    return torch.Tensor(all_cliques)
 
 
-def cliques_graph_and_dual(graph):
+def cliques_graph_and_dual(graph, max_clique_size=None):
     """Function to return the number of cliques of a graph and its complement"""
-    dual_graph = complementary_graph(graph)
-    return cliques_counter(graph), cliques_counter(dual_graph)
+    dual_graph = complement_graph(graph)
+    return cliques_counter(graph, max_clique_size=max_clique_size), \
+           cliques_counter(dual_graph, max_clique_size=max_clique_size)
 
 
 def main(argv):
-    g = random_graph(FLAGS.n_nodes)
+    g = random_graph(FLAGS.n_nodes, FLAGS.n_edges)
     # g = complete_graph(FLAGS.n_nodes)
-    print(cliques_graph_and_dual(g))
+    cliques, complement_cliques = cliques_graph_and_dual(g, FLAGS.max_clique_size)
+    print(cliques)
+    print(complement_cliques)
+    # print(k_cliques(g)[2])
 
 
 if __name__ == '__main__':
+    logging.set_verbosity(logging.INFO)
     app.run(main)
