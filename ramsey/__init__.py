787fee532bd6fd666bc3efb1469550535757724f
bajoukx <jose.a.d@gmail.com>
2022-08-11 01:22:26 +0200

    MAINT. gym register functions.

--- ramsey/__init__.py
+++ ramsey/__init__.py
@@ -4,5 +4,10 @@ from gym.envs.registration import register
 
 register(
     id='RamseyGame-v0',
-    entry_point='ramsey.envs:RamseyGame',
+    entry_point='ramsey.envs:RamseyGame'
+)
+
+register(
+    id='RamseyGame-v1',
+    entry_point='ramsey.envs:RamseyGameMultiplayer'
 )
--- ramsey/envs/__init__.py
+++ ramsey/envs/__init__.py
@@ -1,3 +1,4 @@
 """Imports the RamseyGame environment."""
 
 from ramsey.envs.ramsey_env import RamseyGame
+from ramsey.envs.ramsey_env_multiplayer import RamseyGameMultiplayer
